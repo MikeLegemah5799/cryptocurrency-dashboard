740810c8c26368c0fd31c81a9447b51d5004d145
Michael Legemah <michaellegemah@gmail.com>
2019-12-03 17:53:30 -0500

    added in fonts

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import Welcome from "./Components/welcome/welcome-message.component";
 
 const MyButton = styled.div`
   color: green;
+  ${props =>
+    props.primary &&
+    css`
+      color: palevioletred;
+    `}
 `;
 
 class App extends Component {
@@ -15,6 +20,7 @@ class App extends Component {
       <div>
         <Welcome />
         <MyButton>Hello</MyButton>
+        <MyButton primary>Hello</MyButton>
       </div>
     );
   }
