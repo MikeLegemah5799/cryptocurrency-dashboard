576a2ec3b564d35c6f406c99593b5b6898cbd0c3
Michael Legemah <michaellegemah@gmail.com>
2019-12-03 19:04:40 -0500

    added in AppBar component

--- src/App/Components/welcome/welcome-message.component.jsx
+++ src/App/Components/welcome/welcome-message.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Welcome = () => {
-  return <h1>Hi, Welcome to your Cryptocurrency Dashboard!</h1>;
+  return <h1>Hi, Welcome To Your Cryptocurrency Dashboard!</h1>;
 };
 
 export default Welcome;
--- src/App/index.js
+++ src/App/index.js
@@ -1,27 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-
-import styled, { css } from "styled-components";
-
 import Welcome from "./Components/welcome/welcome-message.component";
-
-const MyButton = styled.div`
-  color: green;
-  ${props =>
-    props.primary &&
-    css`
-      color: palevioletred;
-    `}
-`;
+import AppLayout from "./Components/AppLayout/AppLayout";
+import AppBar from "./Components/AppBar/AppBar";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
+        <AppBar />
         <Welcome />
-        <MyButton>Hello</MyButton>
-        <MyButton primary>Hello</MyButton>
-      </div>
+      </AppLayout>
     );
   }
 }
