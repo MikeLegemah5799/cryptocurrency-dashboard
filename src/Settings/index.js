6ec3a5bc45d6e63d961d75b5422271a8ed9cb7eb
Michael Legemah <michaellegemah@gmail.com>
2019-12-05 16:22:10 -0500

    added in coingraid

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import Welcome from "./welcome-message.component";
 import ConfirmButton from "./ConfirmButton";
 import Page from "../Shared/Page";
+import CoinGrid from "./CoinGrid";
 
 export default function() {
   return (
     <Page name="settings">
-      {" "}
       <Welcome />
       <ConfirmButton />
+      <CoinGrid />
     </Page>
   );
 }
