111eeff21e0465206e168ea73f2568b1a23fd0af
Michael Legemah <michaellegemah@gmail.com>
2019-12-05 15:24:48 -0500

    set up page component

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Welcome from "./welcome-message.component";
 import ConfirmButton from "./ConfirmButton";
+import Page from "../Shared/Page";
 
 export default function() {
   return (
-    <div>
+    <Page name="settings">
       {" "}
-      <Welcome /> <ConfirmButton />
-    </div>
+      <Welcome />
+      <ConfirmButton />
+    </Page>
   );
 }
