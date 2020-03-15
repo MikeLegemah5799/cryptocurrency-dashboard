fec21c92682ac43467f4fbd4908f6cc993dd6991
Michael Legemah <michaellegemah@gmail.com>
2020-03-15 18:44:14 -0400

    build files

---
+++ build/precache-manifest.5bc9c7b550dd41c1dba29b7b164e87ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ebaafdb4f0bf79669052d5c837a3d26",
+    "url": "/cryptocurrency-dashboard/index.html"
+  },
+  {
+    "revision": "5ebea031bffce7c6803a",
+    "url": "/cryptocurrency-dashboard/static/css/main.3dcd270b.chunk.css"
+  },
+  {
+    "revision": "43686b116ae70de4bd42",
+    "url": "/cryptocurrency-dashboard/static/js/2.1fd76920.chunk.js"
+  },
+  {
+    "revision": "5ebea031bffce7c6803a",
+    "url": "/cryptocurrency-dashboard/static/js/main.83f0f9ea.chunk.js"
+  },
+  {
+    "revision": "30f52432ae4881ebfcc8",
+    "url": "/cryptocurrency-dashboard/static/js/runtime-main.d3ca9e09.js"
+  }
+]);
